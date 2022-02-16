d81dfdb46a93114ac75ffac8cfc3fa3fa5a619a0
unknown <sabrinapires_@outlook.com>
2022-02-16 18:31:24 -0300

    strings-arrays-atualizado

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -21,11 +21,28 @@ SUBI NUM ÔNIBUS EM MIRRICOS 27
 
 //console.log ("O e-mail", email , "foi cadastrado com sucesso. Seja bem-vinda(o)" , nome)
 
-//const comidas = ['Doces','Pizza', 'Lanche', 'Sushi', 'Macarrão']
-//console.log(comidas)
-//const frase = "Essas são as minhas comidas preferidas:"
+const comidas = ['Doces','Pizza', 'Lanche', 'Sushi', 'Macarrão']
+console.log(comidas)
+const frase = "Essas são as minhas comidas preferidas:"
+console.log(frase)
+const doce = comidas [0]
+console.log(doce)
+const pizza = comidas [1]
+console.log(pizza)
+const lanche = comidas [2]
+console.log(lanche)
+const sushi = comidas [3]
+console.log(sushi) 
+const macarrao = comidas [4]
+console.log(macarrao)
 
+const comidaUsuario = prompt ("Qual sua comida favorita")
+comidas.push(comidaUsuario)
+comidas.splice(1, 0 ,  comidaUsuario)
+console.log(comidas)
+ //trocar a comida dois da minha lista pela comida do usuario
 
+/*
 const listaDeTarefas = []
 
 const tarefa1 = prompt ("Escreva sua tarefa 1")
@@ -38,11 +55,9 @@ listaDeTarefas.push(tarefa3)
 
 console.log(listaDeTarefas)
 
-const tarefaConcluida = prompt("Digite de 0 a 3 o numero referente a sua tarefa concluida")
+const tarefaConcluida = prompt("Digite de 0 a 2 o numero referente a sua tarefa concluida")
 
-tarefaConcluida.splice(listaDeTarefas ,  0, 1)
+listaDeTarefas.splice(tarefaC, 1)
 
-console.log(tarefaConcluida)
-
-
-
+console.log(listaDeTarefas)
+*/
