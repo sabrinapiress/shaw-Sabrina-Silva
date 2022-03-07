530c2638cf13f1d92299d427f64fb25bce4edca9
sabrinapiress <sabrinapires_@outlook.com>
2022-03-07 19:39:05 -0300

    EXERCICIOS-REVISAO-JS

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -21,39 +21,66 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    let verificarPar = function(numero){ 
+       return numero % 2 === 0
+    }   
+    //let numerosPares = array.filter(verificarPar) 
+   return array.filter(verificarPar)
 }
-//let numeroPar = Number(prompt("Insira um numero PAR"))
-// let restoDiv = numeroPar % 2
-//     console.log(restoDiv)
+
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- array.filter((par))
+    let verificarPar = function(numero){ 
+        return numero % 2 === 0
+     }  
+     let numerosPares = numero * 2
+    return array.filter(numerosPares)
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
-}
+const maiorNumero = Math.max(...array)
+    return maiorNumero
+} 
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+    let maiorNumero = Math.max(num1, num2)
+    let menorNumero = Math.min(num1, num2)
+    let maiorDivisivelPorMenor = maiorNumero % menorNumero === 0
+    let diferenca = maiorNumero % menorNumero
+    const objeto = {
+       maiorNumero:  maiorNumero,
+       maiorDivisivelPorMenor: maiorDivisivelPorMenor,
+       diferenca: diferenca
+    }
+        return objeto
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let par = []
+
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if(ladoA && ladoB && ladoC){
+        return "Equilátero"
+    }else if( ladoA || ladoB || ladoC){
+        return "Isósceles"
+    }else if(ladoA !== ladoB !== ladoC){
+        return "Escaleno"
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    array.sort((a, b)=> a - b)
+    const segundoNumeroMenor = array[1]
+    const segundoNumeroMaior = array[array.length - 2]
+    return [segundoNumeroMaior,segundoNumeroMenor]
+
 }
 
 // EXERCÍCIO 11
