8840dcee9d9754288ec76f285192267d71f73e45
Sabrina Pires <sabrinapires_@outlook.com>
2022-03-25 23:09:41 -0300

    Primeira atualização com codigo

--- modulo2/whatsApp/projeto/src/App.js
+++ modulo2/whatsApp/projeto/src/App.js
@@ -1,41 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+//import "./App.css";
 import React from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Input = styled.input`
-`
+const WhatsLab = styled.div `
+font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+`;
+const Chat = styled.div ``;
+const Input = styled.div ``;
 
+class App extends React.Component {
+  state = {
+    chat: [],
+    valorInputNome: "",
+    valorInputMsg: "",
+    onclick: false,
+  };
 
- class App extends React.Component {
-    state = {
-        chat: [
-            {
-
-            },
-        ],
-        valorInputNome: "",
-        valorInputMsg: "",
-        onclick: false
+  conversa = () => {
+    const pessoa = {
+      nome: this.state.valorInputNome,
+      mensagem: this.state.valorInputMsg,
     };
 
-    novaPessoa = () => {
-        const pessoa = {
-            nome: this.state.valorInputNome,
-            mensagem: this.state.valorInputMsg
-        };
+    const adicionaConversa = [...this.state.chat, pessoa];
 
-        const adicionaPessoa = [...this.state.chat.pessoa];
-        this.setState({chat:pessoa, onclick: true});
-    }
-    render(){
+    //this.setState({chat:pessoa, onclick: true});
+    this.setState({ chat: adicionaConversa });
+    this.setState({ onChangeInputNome: "" });
+    this.setState({ onChangeInputMsg: "" });
+  };
+
+  onChangeInputNome = (event) => {
+    this.setState({ valorInputNome: event.target.value });
+  };
+
+  onChangeInputMsg = (event) => {
+    this.setState({ valorInputMsg: event.target.value });
+  };
+  render() {
+    let listaDeComponentes = this.state.chat.map((pessoa) => {
+      if (this.state.onclick) {
+        return (
+          <div>
+            <spam>{pessoa.nome}</spam>:<spam>{pessoa.mensagem}</spam>
+          </div>
+        );
+      }
+    });
 
     return (
-        <div>
-           <input></input>
-        </div>
+      <WhatsLab>
+        <Chat>
+          <div>{listaDeComponentes}</div>
+          <Input>
+            <input type= "text" className="inputNome">
+              value={this.state.valorInputNome}
+              onChangeInputNome={this.onChangeInputNome}
+              placeholder={"Nome"}
+            </input>
+            <input type= "text" className="inputMsg">
+              value={this.state.valorInputMsg}
+              onChangeInputNome={this.onChangeInputMsg}
+              placeholder={"Mensagem"}
+            </input>
+          </Input>
+          <button onClick={this.conversa}>Enviar</button>
+        </Chat>
+        </WhatsLab>
     );
-}
+  }
 }
 
 export default App;
