1095874da1a9b83e8c24e81b4df86abe3829b1b1
Sabrina Pires <sabrinapires_@outlook.com>
2022-04-08 16:23:35 -0300

    08/04/2022

--- modulo2/projeto-labefy/labefy-projeto/src/Componentes/TelaCriaPlaylist.js
+++ modulo2/projeto-labefy/labefy-projeto/src/Componentes/TelaCriaPlaylist.js
@@ -3,81 +3,66 @@ import React from "react";
 import styled from "styled-components";
 
 const headers = {
-    headers: {
-      Authorization: "sabrina-pires-shaw",
-    },
-  };
+  headers: {
+    Authorization: "sabrina-pires-shaw",
+  },
+};
 
 export default class TelaCriaPlaylist extends React.Component {
-    state = {
-      inputPlaylist: ""
-    };
-  
-    onChangePlaylist = (eve) => {
-      this.setState({
-        inputPlaylist: eve.target.value,
-      });
+  state = {
+    inputPlaylist: "",
+  };
+
+  onChangePlaylist = (eve) => {
+    this.setState({
+      inputPlaylist: eve.target.value,
+    });
+  };
+
+  postCreatePlaylist = () => {
+    const body = {
+      name: this.state.inputPlaylist,
     };
 
-  
-    postCreatePlaylist = () => {
-      const body = {
-        name: this.state.inputPlaylist,
-      };
-  
-      const url =
-        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
-  
-      axios
-        .post(url, body, headers)
-        .then((res) => {
-          alert("Playlist criada. Arrasooou gatah(oh)!!!");
-  
-         //this.getAllPlaylist()
-  
-          this.setState({
-            inputPlaylist: "",
-          });
-        })
-        .catch((err) => {
-          alert("Erro. Tente novamente!");
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
+
+    axios
+      .post(url, body, headers)
+      .then((res) => {
+        alert("Playlist criada. Arrasooou gatah(oh)!!!");
+
+        //this.getAllPlaylist()
+
+        this.setState({
+          inputPlaylist: "",
         });
-    };
-  
-    render() {
-      return (
+      })
+      .catch((err) => {
+        alert("Erro. Tente novamente!");
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Crie sua Playlist</h2>
         <div>
-          <table>
-            <h2>Crie sua Playlist</h2>
-            <div>
-              <td>
-                <tr>
-                  <input
-                    placeholder={"Nome Playlist"}
-                    value={this.state.inputPlaylist}
-                    onChange={this.onChangePlaylist}
-                  />
-                </tr>
-              </td>
-            </div>
-            <div>
-              <td>
-                <tr>
-                  <button onClick={this.postCreatePlaylist}>Criar</button>
-                </tr>
-              </td>
-            </div>
-            <div>
-              <td>
-                <tr>
-                  <button onClick={this.props.irParaDetalhePlaylist}>
-                    Detalhes Playlist
-                  </button>
-                </tr>
-              </td>
-            </div>
-          </table>
+          <input
+            placeholder={"Nome Playlist"}
+            value={this.state.inputPlaylist}
+            onChange={this.onChangePlaylist}
+          />
         </div>
-      );
-    }
+        <div>
+          <button onClick={this.postCreatePlaylist}>Criar</button>
+        </div>
+        <div>
+          <button onClick={this.props.irParaDetalhePlaylist}>
+            Detalhes Playlist
+          </button>
+        </div>
+      </div>
+    );
   }
+}
--- modulo2/projeto-labefy/labefy-projeto/src/Componentes/TelaDetalhesPlaylist.js
+++ modulo2/projeto-labefy/labefy-projeto/src/Componentes/TelaDetalhesPlaylist.js
@@ -3,70 +3,122 @@ import axios from "axios";
 import styled from "styled-components";
 
 const headers = {
-    headers: {
-      Authorization: "sabrina-pires-shaw",
-    },
-  };
+  headers: {
+    Authorization: "sabrina-pires-shaw",
+  },
+};
 
 export default class TelaDetalhesPlaylist extends React.Component {
-    state = {
-      playlist: [],
-    };
-  
-    // componentDidMount() {
-    //   this.getAllPlaylist();
-    // }
-  
-    getAllPlaylist() {
-      const url =
-        "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
-  
-      axios
-        .get(url, headers)
-        .then((res) => {
-          this.setState({
-            playlist: res.data,
-          });
-          console.log(res.data);
-        })
-        .catch((err) => {
-          alert(err.response);
-        });
-    }
-  
-    deletePlaylist = (playlistId) => {
-      const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`;
-  
-      axios
-        .delete(url, headers)
-        .then((res) => {
-          alert("Usuario(a) deletado(a) com sucesso!");
-  
-          this.getAllPlaylist();
-        })
-        .catch((err) => {
-          alert("Erro");
-        });
-    };
-  
-    render() {
-      const renderPlaylist = this.state.playlist.map((playlists) => {
-        return (
-          <div key={playlists.id}>
-            <li>
-              {playlists.name}
-              <button onClick={() => this.deletePlaylist(playlists.id)}>X</button>
-            </li>
-          </div>
-        );
+  state = {
+    playlist: [],
+    inputNomeMusica: "",
+    inputArtista: "",
+    inputUrl: "",
+  };
+
+  getAllPlaylists = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
+    axios
+      .get(url, headers)
+      .then((response) => {
+        this.setState({ playlist: response.data.result.list });
+      })
+      .catch((error) => {
+        alert(error, "Ocorreu um problema, tente novamente");
       });
-  
+  };
+
+  postAddTrackToPlaylist = (playlistId) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}/tracks`;
+
+    const body = {
+      name: this.state.inputNomeMusica,
+      artist: this.state.inputArtista,
+      url:this.state.inputUrl,
+    }
+    .post(url, body, headers);
+  };
+
+  deletePlaylist = (playlistId) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`;
+
+    axios
+      .delete(url, headers)
+      .then((res) => {
+        alert("Playlist deletada!");
+      })
+      .catch((err) => {
+        alert(err, "Erro!");
+      });
+  };
+  componentDidMount() {
+    this.getAllPlaylists();
+  }
+
+  componentDidUpdate() {
+    this.getAllPlaylists();
+  }
+
+  onChangeNomeMusica = (eve) => {
+    this.setState({
+      inputNomeMusica: eve.target.value,
+    });
+  };
+
+  onChangeArtista = (eve) => {
+    this.setState({
+      inputArtista: eve.target.value,
+    });
+  };
+
+  onChangeUrl = (eve) => {
+    this.setState({
+      inputUrl: eve.target.value,
+    });
+  };
+
+  render() {
+    const renderPlaylist = this.state.playlist.map((playlists) => {
       return (
-        <div>
-          <h3>Sua Playlist</h3>
-          <ul>{renderPlaylist}</ul>
-          <button onClick={this.props.irParaCriarPlaylist}>Criar nova Playlist</button>
+        <div key={playlists.id}>
+          <li>
+            {playlists.name}
+            <button onClick={() => this.deletePlaylist(playlists.id)}>X</button>
+          </li>
+          <div>
+            <input
+              placeholder={"Musica"}
+              value={this.state.inputNomeMusica}
+              onChange={this.onChangeNomeMusica}
+            />
+          </div>
+          <div>
+            <input
+              placeholder={"Artista"}
+              value={this.state.inputArtista}
+              onChange={this.onChangeArtista}
+            />
+          </div>
+          <div>
+            <input
+              placeholder={"Faixa"}
+              value={this.state.inputUrl}
+              onChange={this.onChangeUrl}
+            />
+          </div>
         </div>
       );
-    }
+    });
+
+    return (
+      <div>
+        <h3>Sua Playlist</h3>
+        <ul>{renderPlaylist}</ul>
+        <button onClick={this.props.irParaCriarPlaylist}>
+          Criar nova Playlist
+        </button>
+      </div>
+    );
   }
+}
