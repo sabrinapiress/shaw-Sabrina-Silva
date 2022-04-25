069c03682b518c5853dcaad2f2a49097049fc3f1
Sabrina Pires <sabrinapires_@outlook.com>
2022-04-25 16:31:54 -0300

    Tentativa de fazer a lista funcionar

--- modulo3/projeto-AstroMatch/astromatch/README.md
+++ modulo3/projeto-AstroMatch/astromatch/README.md
@@ -4,7 +4,7 @@ Status do Projeto: Concluido ✔️
 Descrição do Projeto
 O astromatch é uma versão simplificada de um app de relacionamentos, onde o usuário pode dar match com outros usuários. O objetivo desse projeto foi realizar a integração com a API, que permite a interação com os usuários previamente cadastrados.
 
-Link do Surge: optimal-pie.surge.sh
+Link do Surge: http://optimal-pie.surge.sh
 
 Features
  Botões de dar ou não match com outros usuários
--- modulo3/projeto-AstroMatch/astromatch/src/Components/MatchList/MatchList.js
+++ modulo3/projeto-AstroMatch/astromatch/src/Components/MatchList/MatchList.js
@@ -9,8 +9,9 @@ const MatchList = () => {
 
    const getMatches = () => {
     axios
-    .get (`${url}matches`)
+    .get ("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/sabrina-shaw/matches")
     .then ((res)=>{
+        console.log(res)
         setMatchList(res.data.matches)
     }) 
     .catch ((err)=>{
@@ -33,23 +34,25 @@ const MatchList = () => {
       })
   }
 
-  const renderlist = matchList.map((match)=>{
-      return(
-          <div key={match.id}>
-              <img src={match.photo} alt= {match.name}/>
-              <p>{match.name}</p>
-          </div>
-      )
-  })
-//   console.log(matchList);
+const List = matchList.response
+let renderList
+ renderList = List ? matchList.response.map((match)=>{
+    return(
+        <div key={match.id}>
+            <img src={match.photo} alt= {match.name}/>
+            <p>{match.name}</p>
+        </div>
+    )
+}): renderList = <p>Erro! Sem matches</p>
 
+//console.log(renderList);
     return (
         <Container>
             <>
             <TitleH3>Seus Matches</TitleH3>
             </>
-            {renderlist}
-            <button onClick={putClear}>Excluir matches</button>
+            {renderList}
+            <button onClick={()=> putClear()}>Excluir matches</button>
         </Container>
     )
 }
