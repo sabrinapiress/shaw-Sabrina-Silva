b6c1de533ba2ffad44f4a62894e71b8a0262533e
Sabrina Pires <sabrinapires_@outlook.com>
2022-05-25 16:56:28 -0300

    teste

--- modulo4/intro-typescript/src/exercicio7.ts
+++ modulo4/intro-typescript/src/exercicio7.ts
@@ -4,3 +4,9 @@ const rna: string= "UAACGACGCGUAAUUGCUGCGCAU"
 const resultado = dna.replace("ATTGCTGCGCATTAACGACGCGTA","UAACGACGCGUAAUUGCUGCGCAU")
 
 console.log(resultado);
+
+function dnaParaRna(dna:string):string{
+return dna.replace(/A/g, "U").replace(/T/g,"A").replace(/G/g,"s").replace(/C/g,"G").replace(/s/g,"C")
+}
+
+console.log(dnaParaRna("ATTGCTGCGCATTAACGACGCGTA"));
