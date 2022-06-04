3097ea3fd54579a59100316345d09dd44e8d5e55
Sabrina Pires <sabrinapires_@outlook.com>
2022-06-04 13:14:19 -0300

    04/06/2022

--- modulo4/IWFS-Sistema-Bancario/Sistema-Bancario/src/index.ts
+++ modulo4/IWFS-Sistema-Bancario/Sistema-Bancario/src/index.ts
@@ -50,30 +50,42 @@ app.post("/conta/criar-conta", (req, res) => {
       //     descricao:""
       // }
     ]
-    if(data < 18){
-      throw new Error("Idade não autorizada")
+
+    if (req.body.nome !== String(req.body.nome) || req.body.cpf !== Number(req.body.cpf) || req.body.nascimento !== String(req.body.nascimento)) {
+      throw new Error("Campo com informação invalida, verifique o conteúdo.")
+    }
+    if (data < 18) {
+      throw new Error("Idade não autorizada.")
     }
     pessoas.push({ nome, cpf, nascimento, saldo, extrato })
-
+    if (!pessoas) {
+      throw new Error("Contas não encontradas.")
+    }
     res.status(200).send(pessoas)
   } catch (error: any) {
     switch (error.message) {
-      case "Idade não autorizada":
+      case "Campo com informação invalida, verifique o conteúdo.":
+        res.status(422)
+        break
+      case "Idade não autorizada.":
         res.status(403)
+        break
+      case "Contas não encontradas.":
+        res.status(400)
         break
       default:
         res.status(500)
-        error.message("Erro inesperado de servidor")
+        error.message("Erro inesperado de servidor.")
         break
     }
     res.send(error.message)
-  } 
+  }
 })
 
 app.get("/saldo/:cpf", (req, res) => {
   try {
     const cpf: number = Number(req.params.cpf)
-    if(!cpf){
+    if (!cpf || cpf !== Number(req.params.cpf)) {
       throw new Error("CPF invalido.")
     }
     const filterPessoa = pessoas.filter((pessoa) => {
@@ -81,16 +93,29 @@ app.get("/saldo/:cpf", (req, res) => {
         return pessoa
       }
     })
+    if (!filterPessoa) {
+      throw new Error("Conta não encontrada.")
+    }
 
-    const saldo = filterPessoa.map((saldo)=>{
+    const saldo = filterPessoa.map((saldo) => {
       return saldo.saldo
     })
+
+    if (!saldo) {
+      throw new Error("Saldo não encontrado.")
+    }
 
     res.status(200).send(saldo)
   } catch (error: any) {
     switch (error.message) {
       case "CPF invalido.":
         res.status(422)
+        break
+      case "Conta não encontrada.":
+        res.status(400)
+        break
+      case "Saldo não encontrado.":
+        res.status(400)
         break
       default:
         res.status(500)
@@ -101,21 +126,71 @@ app.get("/saldo/:cpf", (req, res) => {
   }
 })
 
-app.put("/deposito", (req,res)=>{
-  try{
-    const {nome, cpf, valor} = req.body
-    const pessoasFilter = pessoas.filter((pessoa)=>{
-      if(nome === pessoa.nome && cpf === pessoa.cpf)
-      return pessoa
+app.put("/deposito", (req, res) => {
+  try {
+    const { nome, cpf, valor } = req.body
+    const extratoDeposito = {
+      valor: valor,
+      data: Date(),
+      descricao: "Depósito de dinheiro"
+    }
+
+    const pessoasFilter:typeof pessoas = pessoas.filter((pessoa) => {
+      if (nome === pessoa.nome && cpf === pessoa.cpf) {
+        return pessoa
+      }
+    })
+
+    const saldoDeposito = pessoasFilter.map((saldo) => {
+      const novoSaldo = saldo.saldo + valor
+      return novoSaldo
     })
     
-    const saldoDeposito = pessoasFilter.map((saldo)=>{
-      return saldo.saldo + valor
-    })
+    const extrato =  pessoas.filter((user)=>{
+      if (nome === user.nome && cpf === user.cpf) 
+        return user.extrato.push(extratoDeposito) 
+  })
+   
+    //   if (nome !== pessoasFilter.nome || cpf !== pessoasFilter.cpf){
+    //     throw new Error("Nome ou CPF inválidos.")
+    // }
 
-    res.status(200).send(saldoDeposito)
-  }catch(error:any){
+    if (!pessoasFilter) {
+      throw new Error("Conta não encontrada.")
+    }
+    if (req.body.nome !== String(req.body.nome) || req.body.cpf !== Number(req.body.cpf) || req.body.valor !== Number(req.body.valor)) {
+      throw new Error("Campo com informação invalida, verifique o conteúdo.")
+    }
+    // const extratos = saldoDeposito.map((extrato) => {
+      
+    //   return extrato.push(extratoDeposito)
+    // })
 
+    if (!saldoDeposito) {
+      throw new Error("Saldo indisponivel.")
+    }
+
+    res.status(200).send([ saldoDeposito, extrato])
+  } catch (error: any) {
+    switch (error.message) {
+      case "Campo com informação invalida, verifique o conteúdo.":
+        res.status(422)
+        break
+      case "Conta não encontrada.":
+        res.status(400)
+        break
+      case "Saldo indisponivel.":
+        res.status(400)
+        break
+      case "Nome ou CPF inválidos.":
+        res.status(422)
+        break
+      default:
+        res.status(500)
+        error.message("Erro inesperado de servidor.")
+        break
+    }
+    res.send(error.message)
   }
 })
 
